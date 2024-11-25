a78578b6365d72a3491f4ced8bed38add7f3b91f
Arabinda Kumar Nayak <133951645+ArabindakumarNayak@users.noreply.github.com>
2024-11-25 18:40:12 +0530

    25/11/2024

---
+++ com.springcore/src/main/java/com/springcore/stereotype/Test.java
@@ -0,0 +1,15 @@
+package com.springcore.stereotype;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/springcore/stereotype/stereoconfig.xml");
+
+		Student std1 = (Student) ctx.getBean("student");
+		System.out.println(std1);
+
+	}
+}
